6501afd2b44b2d3d8dcbb3f7e5755eb47b7067bd
Atul_Arya <223510116223@aryabhattacollege.ac.in>
2024-03-03 13:42:22 +0530

    student model implemented successfully

--- mysite/home/views.py
+++ mysite/home/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
 
 # Create your views here.
+
 def index(request):
-    return render(request,'index.html',{})
+    students = Student.objects.all()
+    return render(request,'index.html',{'students':students})
 
 def about(request):
     return render(request,'about.html',{})
