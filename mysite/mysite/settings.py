9fa20b515cff2391ed62b042ec3044e3b4ded9bc
Atul_Arya <223510116223@aryabhattacollege.ac.in>
2024-03-03 14:18:38 +0530

    database changed to sqlite3

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -77,6 +77,13 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
+'''
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'Django',    # Specify your MySQL database name
@@ -89,7 +96,7 @@ DATABASES = {
         },
     }
 }
-
+'''
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
